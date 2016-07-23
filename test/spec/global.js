cc39a2eb5d6df3e0616d97719f58d1304d477a66
Miguel Castillo <manchagnu@gmail.com>
2016-07-23 13:57:44 -0400

    Adding support for global object

---
+++ test/spec/global.js
@@ -0,0 +1,60 @@
+import { expect } from "chai";
+import builtinsFactory from "../../index";
+
+var builtins = builtinsFactory();
+
+describe("global test suite", function() {
+  describe("When resolving a module with name global", () => {
+    var act, meta, result;
+
+    beforeEach(() => act = () => result = builtins.resolve(meta));
+
+    describe("and the module name is global", () => {
+      beforeEach(() => {
+        meta = {
+          name: "global"
+        };
+
+        act();
+      });
+
+      it("then result is undefined", () => {
+        expect(result).to.be.undefined;
+      });
+    });
+  });
+
+  describe("When transform a module with global in its source", () => {
+    var act, meta, result;
+
+    beforeEach(() => act = () => result = builtins.transform(meta));
+
+    describe("and the module source has global", () => {
+      beforeEach(() => {
+        meta = {
+          source: " global "
+        };
+
+        act();
+      });
+
+      it("then the global dependency is injected", () => {
+        expect(result.source).to.be.equal("(function(global) {\n global \n})(typeof global !== \'undefined\' ? global : typeof self !== \'undefined\' ? self : typeof window !== \'undefined\' ? window : {})");
+      });
+    });
+
+    describe("and the module source does not have global", () => {
+      beforeEach(() => {
+        meta = {
+          source: " global23 "
+        };
+
+        act();
+      });
+
+      it("then result is undefined", () => {
+        expect(result).to.be.undefined;
+      });
+    });
+  });
+});
