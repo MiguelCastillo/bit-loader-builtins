830588cc7de87899c0236157f878b639b1291fc8
Miguel Castillo <manchagnu@gmail.com>
2016-07-23 11:44:37 -0400

    Integrating with browser-builtins

--- index.js
+++ index.js
@@ -1,54 +1,29 @@
-var resolver = require("bit-bundler-utils/resolvePath").configure({baseUrl: __filename});
+var browserBuiltins = require("browser-builtins");
 var path = require("path");
 
 
-var builtInMap = {
-  buffer: {
-    name: "buffer",
-    target: "buffer",
-    injectDependency: function() {
-      return false;
-    }
-  },
-  events: {
-    name: "events",
-    target: "events",
-    injectDependency: function() {
-      return false;
-    }
-  },
-  path: {
-    name: "path",
-    target: "path",
-    injectDependency: function() {
-      return false;
-    }
-  },
+var dependencyInjectionMap = {
   process: {
-    name: "process",
-    target: "process/browser",
     injectDependency: function(meta) {
-      return /process.(cwd|nextTick|platform)/.test(meta.source) && meta.name !== builtInMap.process.name;
+      return /process.(cwd|nextTick|platform)/.test(meta.source) && meta.name === "process";
     },
-    value: function() {
+    depedencyValue: function() {
       return "require('process')";
     }
   },
   __dirname: {
-    name: "__dirname",
     injectDependency: function(meta) {
       return /\b__dirname\b/.test(meta.source);
     },
-    value: function(meta) {
+    depedencyValue: function(meta) {
       return "'/" + path.relative(".", meta.directory) + "'";
     }
   },
   __filename: {
-    name: "__filename",
     injectDependency: function(meta) {
       return /\b__filename\b/.test(meta.source);
     },
-    value: function(meta) {
+    depedencyValue: function(meta) {
       return "'/" + path.relative(".", meta.path) + "'";
     }
   }
@@ -56,25 +31,28 @@ var builtInMap = {
 
 
 function resolveBuiltin(meta) {
-  if (builtInMap.hasOwnProperty(meta.name)) {
-    return resolver({
-      name: builtInMap[meta.name].target
-    });
+  if (browserBuiltins.hasOwnProperty(meta.name)) {
+    return {
+      path: browserBuiltins[meta.name]
+    };
   }
 }
 
 
-function transformBuiltin(meta) {
+function injectBuiltinDependency(meta) {
   var wrapped;
 
-  var builtInResult = Object.keys(builtInMap).reduce(function(container, builtIn) {
-    if (builtInMap[builtIn].injectDependency(meta)) {
+  var builtInResult = Object
+    .keys(dependencyInjectionMap)
+    .filter(function(builtIn) {
+      return dependencyInjectionMap[builtIn].injectDependency(meta);
+    })
+    .reduce(function(container, builtIn) {
       container.params.push(builtIn);
-      container.deps.push(builtInMap[builtIn].value(meta));
-    }
+      container.deps.push(dependencyInjectionMap[builtIn].depedencyValue(meta));
 
-    return container;
-  }, {params: [], deps: []});
+      return container;
+    }, {params: [], deps: []});
 
   if (builtInResult.params.length) {
     wrapped = "(function(" + builtInResult.params.join(",") + ") {\n";
@@ -82,15 +60,17 @@ function transformBuiltin(meta) {
     wrapped += "\n})(" + builtInResult.deps.join(",") + ")";
   }
 
-  return {
-    source: wrapped || meta.source
-  };
+  if (wrapped) {
+    return {
+      source: wrapped
+    };
+  }
 }
 
 
 module.exports = function() {
   return {
     resolve: resolveBuiltin,
-    transform: transformBuiltin
+    transform: injectBuiltinDependency
   };
 };
